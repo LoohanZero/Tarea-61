e1f278ef8618ec521841f493c442924fb261b119
Lujan Sanchez <mlulasan@gmail.com>
2020-04-17 18:34:41 -0300

    terminado cambio de color del body, falta conversion a hsl

--- RGB/range.js
+++ RGB/range.js
@@ -1,11 +1,29 @@
 const range = document.getElementsByClassName("range-value")
-
 const p = document.getElementsByClassName("actual-value")
+const finalColor = document.getElementById("final-color")
+let bodyBody = document.body; 
+console.log(bodyBody)
+
 
 const addValue = (index)=>{
     p[index].innerHTML = range[index].value
 }
 
+const createFinalValue = () => {
+    finalColor.innerHTML = `rgb(${p[0].innerHTML}, ${p[1].innerHTML}, ${p[2].innerHTML})`
+    changeBodyColor()
+}
+
+const changeBodyColor = () => {
+    bodyBody.style.backgroundColor = finalColor.innerHTML;
+    console.log(bodyBody.style.backgroundColor)
+}
+
 range[0].addEventListener("change", function(){addValue(0)})
 range[1].addEventListener("change", function(){addValue(1)})
 range[2].addEventListener("change", function(){addValue(2)})
+
+
+range[0].addEventListener("change", createFinalValue)
+range[1].addEventListener("change", createFinalValue)
+range[2].addEventListener("change", createFinalValue)
--- Twitter/script.js
+++ Twitter/script.js
@@ -3,32 +3,24 @@ const sendButton = document.getElementsByClassName("send")[0];
 const counter = document.getElementsByTagName("p")[0];
 
 const toggleColor = () => {
-    if(counter.innerHTML < 0) {
+    if(textarea.value.length >= 240) {
         textarea.classList.add("red-letter");
         counter.classList.add("red-letter");
         sendButton.disabled = true;
     }
-    else if (counter.innerHTML >= -1 ) {
+    else if (textarea.value.length <= 241 ) {
         textarea.classList.remove("red-letter");
         counter.classList.remove("red-letter");
         sendButton.disabled = false;
     }
 }
 // (e) => p.innerHTML = 240 - textarea.innerHTML.value
-const characterCounter = (event) => {
-    if(event.keyCode !== 8) {
-        counter.innerHTML = Number(counter.innerHTML) - 1;
-    }
-    else {
+const characterCounter = () => {   
+        counter.innerHTML = 240 - Number(textarea.value.length);   
 
-        if (counter.innerHTML > 240) {
-            counter.innerHTML = 240;
-        } else {
-            counter.innerHTML = Number(counter.innerHTML) + 1;
-        }
-        
+              
     }    
-}
+
 
 const sendInfo = () => {
     textarea.value = "";
@@ -36,6 +28,7 @@ const sendInfo = () => {
     alert("Su texto ha sido enviado");
 }
 
-textarea.addEventListener('keydown', characterCounter);
 textarea.addEventListener('keydown', toggleColor);
+textarea.addEventListener('keydown', characterCounter);
+textarea.addEventListener('keyup', characterCounter);
 sendButton.addEventListener('click', sendInfo);
